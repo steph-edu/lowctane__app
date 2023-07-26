7c066fc38f9c6a984912f3de5a3277e2366abf80
steph-edu <stephanyeduarda302@gmail.com>
2023-07-26 17:40:26 -0300

    check activity

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Index;
+use App\Http\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/contador', Counter::class);
+Route::get('/site', Index::class);
